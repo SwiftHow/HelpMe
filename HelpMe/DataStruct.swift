a7d7b4e0dc7d982fca93f0364493d2a7af5be771
Evilcome <evilcome.com@gmail.com>
2015-08-27 16:15:23 +0800

    add theme and data struct

---
+++ HelpMe/DataStruct.swift
@@ -0,0 +1,9 @@
+//
+//  DataStruct.swift
+//  HelpMe
+//
+//  Created by Wayne on 15/8/27.
+//  Copyright (c) 2015年 SWIFT.HOW. All rights reserved.
+//
+
+import Foundation
---
+++ HelpMe/Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Theme.swift
+//  HelpMe
+//
+//  Created by Wayne on 15/8/27.
+//  Copyright (c) 2015年 SWIFT.HOW. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+    
+    // colors
+    static let primary_color = UIColor(hexString: "#ff8942")
+    static let secondary_color = UIColor.magentaColor()
+    
+    // consts
+}
